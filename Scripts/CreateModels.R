46f85d36e65487d272ccca32f4f3aeb33f40005c
mandrew71 <andrew.mckaskell@stud.uni-goettingen.de>
2024-12-11 13:22:04 +0100

    Added a graph

--- Scripts/CreateModels.R
+++ Scripts/CreateModels.R
@@ -22,6 +22,7 @@
 
 #if(!require(corrr)){install.packages("corrr"); library(corrr)} # for correlations
 if(!require(brms)){install.packages("brms"); library(brms)} # for modelling
+if(!require(ggplot2)){install.packages("ggplot2"); library(ggplot2)} # for modelling
 
 # Get the data
 monkeyData <- readRDS("Data/monkeyData.rds")
@@ -61,6 +62,7 @@ modElev <- brm(occ ~ scaleElevmean + offset(log(Effort)),
                data = monkeyData,
                family = bernoulli(link="logit"),
                warmup = 2000, iter = 4000, chains = 4)
+# CI 0.27     0.79  - significant
 
 conditional_effects(modElev)
 
@@ -69,6 +71,7 @@ modRiver <- brm(occ ~ scaleRiverDist + offset(log(Effort)),
                data = monkeyData,
                family = bernoulli(link="logit"),
                warmup = 2000, iter = 4000, chains = 4)
+#CI -0.11     0.39 - not significant
 
 conditional_effects(modRiver)
 
@@ -78,6 +81,7 @@ modRoad <- brm(occ ~ scaleRoadDist + offset(log(Effort)),
                data = monkeyData,
                family = bernoulli(link="logit"),
                warmup = 2000, iter = 4000, chains = 4)
+# CI -0.09     0.41 - not significant
 
 conditional_effects(modRoad)
 
@@ -87,6 +91,7 @@ modVill <- brm(occ ~ scaleVillDist + offset(log(Effort)),
                data = monkeyData,
                family = bernoulli(link="logit"),
                warmup = 2000, iter = 4000, chains = 4)
+# CI -0.05     0.49 - not significant
 
 conditional_effects(modVill)
 
@@ -96,6 +101,7 @@ modRugg <- brm(occ ~ scaleRuggmean + offset(log(Effort)),
                data = monkeyData,
                family = bernoulli(link="logit"),
                warmup = 2000, iter = 4000, chains = 4)
+# CI -0.14     0.37 - not significant
 
 conditional_effects(modRugg)
 
@@ -108,11 +114,143 @@ mc <- add_criterion(modRoad, "loo")
 md <- add_criterion(modVill, "loo")
 me <- add_criterion(modRugg, "loo")
 
-
+# Compare the models with loo_comapre
 loo_compare(m0, ma, mb, mc, md, me, criterion = "loo")
 
+### ma (Elevation) is the best model
+### md (Village distance is next best)
 
-#compare the models with loo_compare
 
-# repeat
+# Create models with elevation plus another covariate
+# Ignore road distance, just use village distance.
+
+# Elevation plus river distance
+modElevRiver <- brm(occ ~ scaleElevmean + scaleRiverDist + offset(log(Effort)),
+                data = monkeyData,
+                family = bernoulli(link="logit"),
+                warmup = 2000, iter = 4000, chains = 4)
+#CI 
+conditional_effects(modElevRiver)
+
+
+# Elevation plus village distance
+modElevVill <- brm(occ ~ scaleElevmean + scaleVillDist + offset(log(Effort)),
+               data = monkeyData,
+               family = bernoulli(link="logit"),
+               warmup = 2000, iter = 4000, chains = 4)
+# CI 
+
+conditional_effects(modElevVill)
+
+
+# Elevation plus ruggedness
+modElevRugg <- brm(occ ~ scaleElevmean + scaleRuggmean + offset(log(Effort)),
+               data = monkeyData,
+               family = bernoulli(link="logit"),
+               warmup = 2000, iter = 4000, chains = 4)
+# CI 
+conditional_effects(modElevRugg)
+
+
+
+
+### Add criterion??
+maa <- add_criterion(modElevRiver, "loo")
+mab <- add_criterion(modElevVill, "loo")
+mac <- add_criterion(modElevRugg, "loo")
+
+# Compare the models with loo_comapre
+loo_compare(m0, ma, maa, mab, mac, criterion = "loo")
+
+# Elevation plus ruggedness is the better model
+
+
+#-------------------------------------
+# Elevation plus ruggedness plus river
+modElevRuggRiver <- brm(occ ~ scaleRuggmean + scaleElevmean + scaleRiverDist + offset(log(Effort)),
+                    data = monkeyData,
+                    family = bernoulli(link="logit"),
+                    warmup = 2000, iter = 4000, chains = 4)
+#CI 
+conditional_effects(modElevRuggRiver)
+
+
+# Elevation plus village distance
+modElevRuggVill <- brm(occ ~ scaleRuggmean + scaleElevmean + scaleVillDist + offset(log(Effort)),
+                   data = monkeyData,
+                   family = bernoulli(link="logit"),
+                   warmup = 2000, iter = 4000, chains = 4)
+# CI 
+
+conditional_effects(modElevRuggVill)
+
+
+##################### TO HERE
+
+maca <- add_criterion(modElevRuggRiver, "loo")
+macb <- add_criterion(modElevRuggVill, "loo")
+
+
+loo_compare(m0, ma, mac, maca, macb, criterion = "loo")
+# elpd_diff se_diff
+# mac   0.0       0.0   - still best model
+# maca -0.1       1.2   
+# macb -0.8       0.8   
+# ma   -1.8       2.3   
+# m0   -8.8       4.4
+
+
+
+
+
+#################################
+# Extract conditional effects
+effects <- conditional_effects(modElevRugg)
+
+# create a plotting dataset
+plotData <- effects[[1]]
+summary(plotData)
+
+M  <- mean(monkeyData$Elevmean)   # Mean of original elevation data
+SD  <- sd(monkeyData$Elevmean)    # SD of original elevation data
+
+# Back transform the sclaed elevation values to natural values
+plotData$Elevation <- M + (plotData$scaleElevmean * SD)
+
+
+(trendPlot <- ggplot(plotData, aes(Elevation)) +
+ geom_ribbon(aes(ymin = lower__, ymax = upper__), fill = "slategrey") +
+ geom_line(aes(y = estimate__), colour="gray0", linewidth=1) +
+    labs(y="Occurrence probability", title="Occurrence probability of Red eared monkey")
+
+  )
+
+# #Plot with the original values on the x axis
+# 
+# h <- ggplot(dt, aes(Rugge)) +
+#   geom_ribbon(aes(ymin = lower__, ymax = upper__), fill = "grey70") +
+#   geom_line(aes(y = estimate__), colour="gray0", linewidth=1)+
+#   theme_bw()+
+#   theme(panel.grid.major.x = element_blank(),
+#         panel.grid.minor.x = element_blank(),
+#         panel.grid.major.y = element_blank(),
+#         panel.grid.minor.y = element_blank())+
+#   labs(x = "Terrain ruggednex index", y = "Occurrence probability")+
+#   theme(axis.text.y = element_text(size = 18, colour="black"), 
+#         axis.text.x = element_text(size = 18, colour="black"), 
+#         axis.title = element_text(face = "plain", size = 18), 
+#         plot.title = element_text(face = "bold", size = 18)) +
+#   scale_y_continuous(limits= c(0.0,0.11,0.030), breaks = seq(0.0,0.11,0.030)) +
+#   #scale_x_continuous(limits= c(0,1100,300), breaks = seq(0,1100,300)) +
+#   facet_wrap(~"Putty-nosed monkey")+
+#   theme(strip.background =element_rect(fill="grey50"))+
+#   theme(strip.text = element_text(colour = 'white'))+
+#   theme(strip.text.x = element_text(size = 18, face="bold"))
+# 
+# h
+# 
+# ggsave(h, file="ElephRugg.png", width = 5, height = 4, dpi=600)
+
+
+
 
